9496c82672318fc19cb8c685222c8ca7582e592f
Jose Zuñiga <josezuniga@MacBook-Pro-de-Jose.local>
2019-01-29 23:10:59 -0300

    Updates

---
+++ precache-manifest.e6519b92e6dd912639b181c7c05a177e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8f669210cadd8df252d69497d793762",
+    "url": "/react-form-validator-cl/static/media/geomanist-light.f8f66921.ttf"
+  },
+  {
+    "revision": "3191278439dbfd8d68b6",
+    "url": "/react-form-validator-cl/static/css/main.049bb7f4.chunk.css"
+  },
+  {
+    "revision": "d81c33b862542dc15129",
+    "url": "/react-form-validator-cl/static/js/1.d81c33b8.chunk.js"
+  },
+  {
+    "revision": "ede7f9106b235e3c1e0b",
+    "url": "/react-form-validator-cl/static/js/runtime~main.ede7f910.js"
+  },
+  {
+    "revision": "df763a25334014196aac650fcd5cd87e",
+    "url": "/react-form-validator-cl/static/media/geomanist-light.df763a25.woff2"
+  },
+  {
+    "revision": "5f153216531bcf695655cb9b3e14ae4b",
+    "url": "/react-form-validator-cl/static/media/geomanist-light.5f153216.woff"
+  },
+  {
+    "revision": "faf738eddd497b22ae4ad22872ac0c44",
+    "url": "/react-form-validator-cl/static/media/geomanist-book.faf738ed.woff"
+  },
+  {
+    "revision": "af0a155c245a77731415caedb5fc97c3",
+    "url": "/react-form-validator-cl/static/media/geomanist-book.af0a155c.woff2"
+  },
+  {
+    "revision": "65ad1b9230dd898aeb4fad8ebbcdbc86",
+    "url": "/react-form-validator-cl/static/media/geomanist-medium.65ad1b92.woff2"
+  },
+  {
+    "revision": "011ac0cb6c89dad091c16207c7fcc323",
+    "url": "/react-form-validator-cl/static/media/geomanist-medium.011ac0cb.woff"
+  },
+  {
+    "revision": "3191278439dbfd8d68b6",
+    "url": "/react-form-validator-cl/static/js/main.31912784.chunk.js"
+  },
+  {
+    "revision": "74552e24965cf285496d020232e3dc70",
+    "url": "/react-form-validator-cl/static/media/geomanist-book.74552e24.eot"
+  },
+  {
+    "revision": "7723212dad4f8bbde627df5497d2446c",
+    "url": "/react-form-validator-cl/static/media/geomanist-medium.7723212d.eot"
+  },
+  {
+    "revision": "a038c6533735e9c264d6afacfcf9b2c7",
+    "url": "/react-form-validator-cl/static/media/geomanist-book.a038c653.ttf"
+  },
+  {
+    "revision": "01dc58af1b8c6be371e362373517d887",
+    "url": "/react-form-validator-cl/static/media/geomanist-light.01dc58af.eot"
+  },
+  {
+    "revision": "0af461c02bd7896a834ef74e1c893a72",
+    "url": "/react-form-validator-cl/static/media/geomanist-medium.0af461c0.ttf"
+  },
+  {
+    "revision": "b4ce6395ad5d996b4182a449d9955c01",
+    "url": "/react-form-validator-cl/static/media/geomanist-light.b4ce6395.svg"
+  },
+  {
+    "revision": "c0948001981ff9d98fb87353f9d07a9d",
+    "url": "/react-form-validator-cl/static/media/geomanist-book.c0948001.svg"
+  },
+  {
+    "revision": "b2e12180a25eaee11e9962c22165d89c",
+    "url": "/react-form-validator-cl/static/media/geomanist-medium.b2e12180.svg"
+  },
+  {
+    "revision": "f78c54aee9106b0966dcb8ce3c1b633c",
+    "url": "/react-form-validator-cl/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76f7661e7491c4d6e9773aa9de59e265.js"
+  "/react-form-validator-cl/precache-manifest.e6519b92e6dd912639b181c7c05a177e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-form-validator-cl/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
